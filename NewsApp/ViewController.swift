0418947e32aef8bd67928efdd0cb803b11430e69
Ajay Dhomase <ajay.dhomase@baseinformation.com>
2021-01-22 11:51:48 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ajay Dhomase on 22/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
